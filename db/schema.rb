f764cda1517bfbaf901158c7d157046b4cce5b57
EC2 Default User <ec2-user@ip-172-31-10-73.ec2.internal>
2022-04-18 12:42:11 +0000

    Fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_18_102247) do
+ActiveRecord::Schema.define(version: 2022_04_18_123942) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -49,9 +49,41 @@ ActiveRecord::Schema.define(version: 2022_04_18_102247) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "end_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postcode"
+    t.string "address"
+    t.string "phone_number"
+    t.boolean "is_deleted"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_end_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
+  end
+
   create_table "order_details", force: :cascade do |t|
-    t.integer "products_id"
-    t.integer "orders_id"
+    t.integer "product_id"
+    t.integer "order_id"
     t.integer "tax_price"
     t.integer "quantity"
     t.integer "production_status"
