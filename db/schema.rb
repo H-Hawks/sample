20fd6984b68d36c6eaeec4de0a31828af7aef950
EC2 Default User <ec2-user@ip-172-31-15-118.ap-northeast-1.compute.internal>
2022-04-19 08:19:51 +0000

    START asmin/products

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_04_18_123942) do
-
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -64,11 +62,6 @@ ActiveRecord::Schema.define(version: 2022_04_18_123942) do
   end
 
   create_table "end_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.string "last_name"
     t.string "first_name"
     t.string "last_name_kana"
@@ -76,24 +69,13 @@ ActiveRecord::Schema.define(version: 2022_04_18_123942) do
     t.string "postcode"
     t.string "address"
     t.string "phone_number"
+    t.string "email"
+    t.string "encrypted_password"
     t.boolean "is_deleted"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_end_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
 
-
-  create_table "order_details", force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "order_id"
-    t.integer "tax_price"
-    t.integer "quantity"
-    t.integer "production_status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-    
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -104,6 +86,16 @@ ActiveRecord::Schema.define(version: 2022_04_18_123942) do
     t.integer "product_id"
     t.integer "end_user_id"
     t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "order_id"
+    t.integer "tax_price"
+    t.integer "quantity"
+    t.integer "production_status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -131,18 +123,6 @@ ActiveRecord::Schema.define(version: 2022_04_18_123942) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
+end
